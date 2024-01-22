9c591609bab40cdb7b48f1dd8e220017bb344589
tomas <tomas.vicente.7e5@itb.cat>
2024-01-22 12:17:55 +0100

    closes #3

--- pop_TVC/Program.cs
+++ pop_TVC/Program.cs
@@ -4,19 +4,32 @@ public class PopProject
 {
     public static void Main(string[] args)
     {
+        // VARIABLES DE LOS 2 EJERCICIOS //
         const string DateInput = "Introdueix el dia, mes i any separats per enters";
         const string DateKo = "El format no és correcte";
         const string DateOk = "La data és correcta";
         int day = 0, month = 0, year = 0;
-        bool dateChecked;
+        bool dateChecked, done;
+        string choice = "";
+
+        // EJERCICIO 1 FECHAS //
         Console.WriteLine(DateInput);
         day = Convert.ToInt32(Console.ReadLine());
         month = Convert.ToInt32(Console.ReadLine());
         year = Convert.ToInt32(Console.ReadLine());
         dateChecked = CheckDate(day, month, year);
         Console.WriteLine(dateChecked ? DateOk : DateKo);
+
+        // EJERCICIO 2 MENU //
+        do
+        {
+            ShowMenu();
+            choice = Console.ReadLine();
+            done = CheckMenuInput(choice);
+        }while(!done);
     }
 
+    // MÉTODO EJ 1 //
     public static bool CheckDate(int day, int month, int year)
     {
         bool dateChecked = true;
@@ -44,7 +57,7 @@ public class PopProject
                     totalDaysMonth = 29;
 
                 else totalDaysMonth = 28;
-            break;
+                break;
             case 3:
                 totalDaysMonth = 31; break;
             case 5:
@@ -63,7 +76,41 @@ public class PopProject
         if (day > totalDaysMonth)
         {
             dateChecked = false;
-        }     
+        }
         return dateChecked;
+    }
+
+    // MÉTODOS EJ 2 //
+    public static void ShowMenu()
+    {
+        const string Menu = "Que quieres hacer: | A - Saltar B - Correr C - Agacharse D - Esconderse|";
+        Console.WriteLine(Menu);
+    }
+    public static bool CheckMenuInput(string choice)
+    {
+        const string Jump = "Salto";
+        const string Run = "Corro";
+        const string Crouch = "Me agacho";
+        const string Hide = "Me escondo";
+        const string Wrong = "Has puesto una opción que no está";
+
+        switch (choice.ToUpper())
+        {
+            case "A":
+                Console.WriteLine(Jump);
+                return true;
+            case "B":
+                Console.WriteLine(Run);
+                return true;
+            case "C":
+                Console.WriteLine(Crouch);
+                return true;
+            case "D":
+                Console.WriteLine(Hide);
+                return true;
+            default:
+                Console.WriteLine(Wrong);
+                return false;
+        }
     }
 }
